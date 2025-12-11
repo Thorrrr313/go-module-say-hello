0ac736d677f126c8ea9b0360137978b67781d80b
thoriq.noer <thoriq.noer@gmail.com>
2025-12-11 14:43:08 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Thorrrr313/go-module-say-hello
+module github.com/Thorrrr313/go-module-say-hello/v2
 
 go 1.24.5
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello world Nur Muhammad Thariq"
+func SayHello(name string) string {
+	return "Hello " + name
 }
